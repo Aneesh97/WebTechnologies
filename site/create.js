ae054d0d58b27ef85b73ef03568836faf799add5
Aneesh <aneeshanand10@gmail.com>
2019-05-17 13:46:01 +0100

    Database connection

--- site/create.js
+++ site/create.js
@@ -8,8 +8,8 @@ async function create() {
         db = await sqlite.open("./data.db");
         await db.run("pragma foreign_keys = on");
         await db.run("create table users (id primary key, username, email, Oscore, Cscore, Escore, Ascore, Nscore, Wellbeing)");
-        await db.run("insert into users values (42,'aa16169', 'aa16169@bristol.ac.uk', 42, 34, 56, 66, 11, 12 )");
-        await db.run("insert into users values (53,'rb16730', 'rb16730@bristol.ac.uk', 11, 45, 7, 21, 13, 45)");
+        await db.run("insert into users values (42, 'aa16169', 'aa16169@bristol.ac.uk', 42, 34, 56, 66, 11, 12 )");
+        await db.run("insert into users values (53, 'rb16730', 'rb16730@bristol.ac.uk', 11, 45, 7, 21, 13, 45)");
 
         await db.run("create table content (contentid primary key, url, category)");
         await db.run("insert into content values (2, 'google.co.uk', 'web browser')");
